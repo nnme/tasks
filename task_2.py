95b0160f4064594f17211b82021bd1884f42a4e2
nnme <makdak69@gmail.com>
2015-11-19 19:00:59 +0300

    second task

--- task_2.py
+++ task_2.py
@@ -4,13 +4,27 @@
 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
 
 Найдите сумму всех четных элементов ряда Фибоначчи, которые не превышают четыре миллиона."""
-#make list with fibinacci 
+#make function for fibinacci 
 def fib(n):
     result = []
-    a, b, = 0, 1
+    a, b, = 1, 0
     while b < n:
         result.append(b)
         a, b = b, a+b
     return result
 
-print fib(1000)
+#make list fib 
+LIST = fib(4000000)
+
+#make list /2
+LIST2 = LIST[::3]
+
+print 'spisok vseh chetnih chisel po fib', LIST2
+
+#make sum from LIST2
+summa = 0
+
+for i in LIST2:
+    summa = i+summa
+
+print 'otvet na zadanie - ',  summa
